dd6d142ca47a8d3e14cbe9c6fae60235d5398643
Macksn <itsmacksn@yahoo.com>
2024-03-24 21:31:47 +0300

    Doviz_Hesabi
    Class ile Döviz Hesabı

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CLASS_BORSA_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        borsa eleman = new borsa();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            eleman.para = Convert.ToInt16(textBox1.Text);
+            if (radioButton1.Checked)
+            {
+                label2.Text = eleman.dolar()+"$";
+            }
+            if (radioButton2.Checked)
+            {
+                label2.Text = eleman.euro()+"EUR";
+            }
+            if (radioButton3.Checked)
+            {
+                label2.Text = eleman.altın()+"gram";
+            }
+                
+        }
+    }
+}
---
+++ borsa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLASS_BORSA_
+{
+    class borsa
+    {
+        public double para;
+        public double euro()
+        {
+            return (para / 3.63);
+        }
+        public double dolar()
+        {
+            return (para / 3.43);
+        }
+        public double altın()
+        {
+            return (para / 131);
+        }
+    }
+}
